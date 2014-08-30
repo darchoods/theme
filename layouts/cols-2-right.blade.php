fd50fae3b75e2a12de3eeb07a3cffc08d1ca1c21
xLink <xLink@cybershade.org>
2014-08-30 13:17:07 +0100

    switched up the conditionals for ie

--- layouts/cols-2-right.blade.php
+++ layouts/cols-2-right.blade.php
@@ -1,6 +1,9 @@
 <!doctype html>
-<!--[if lt IE 9]><html class="ie"><![endif]-->
-<!--[if gte IE 9]><!--><html class=""><!--<![endif]-->
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]> <html class=""> <![endif]-->
 <head>
 <title>{{{ strip_tags(Theme::place('title')) }}}</title>
 <meta charset="utf-8">
