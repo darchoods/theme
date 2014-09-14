cf2b8ca78ee1b207237fe53a8760824f5a7460b9
xLink <xLink@cybershade.org>
2014-09-14 21:33:47 +0100

    fixed typo

--- views/partials/theme/errors/404.blade.php
+++ views/partials/theme/errors/404.blade.php
@@ -1,1 +1,1 @@
-<p>There was an error whislst trying to access the page. Please notify the administrator of this error.</p>
+<p>There was an error whilst trying to access the page. Please notify the administrator of this error.</p>
