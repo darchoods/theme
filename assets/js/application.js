935a7547e2d6d36273baef6c5f1c54305a31ceaa
xLink <xLink@cybershade.org>
2014-09-23 11:56:48 +0100

    added some js init for bs

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,23 @@
-	jQuery(document).ready(function() {
+jQuery(document).ready(function(){
+    initBS();
+    jQuery(document).ajaxSuccess(function(event, request, settings){
+        initBS();
+    });
+});
 
 
-
-	});
+function initBS(){
+    if (jQuery('[data-toggle="tooltip"]').length) {
+        jQuery('[data-toggle="tooltip"]').tooltip();
+    }
+    if (jQuery('[data-toggle="popover"]').length) {
+        jQuery('[data-toggle="popover"]').popover({
+            html: true,
+            trigger: 'hover',
+            content: function(){
+                return jQuery(this).parents('[class*="item"]').find(' > div').text();
+            },
+            placement: 'auto top'
+        });
+    }
+}
